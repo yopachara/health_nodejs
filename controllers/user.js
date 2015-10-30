8f45c7b78a633a73a79bb3ed94a600728c2061ee
Pachara S <pachara.s@mail.kmutt.ac.th>
2015-10-30 23:31:56 +0700

    Change indent

--- controllers/user.js
+++ controllers/user.js
@@ -5,55 +5,55 @@ var moment = require('moment');
 
 // Create endpoint /api/users for POST
 exports.postUsers = function(req, res) {
-  var user = new User({
-    username: req.body.username,
-    password: req.body.password,
-    sex: req.body.sex,
-    weight: req.body.weight,
-    height: req.body.height,
-    birthdate: req.body.birthdate,
-    createAt: req.body.createAt,
-    type: req.body.type,
-    bmr: req.body.bmr,
-    bmi: req.body.bmi,
-    carbo: req.body.carbo,
-    protein: req.body.protein,
-    fat: req.body.fat
-  });
+    var user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        sex: req.body.sex,
+        weight: req.body.weight,
+        height: req.body.height,
+        birthdate: req.body.birthdate,
+        createAt: req.body.createAt,
+        type: req.body.type,
+        bmr: req.body.bmr,
+        bmi: req.body.bmi,
+        carbo: req.body.carbo,
+        protein: req.body.protein,
+        fat: req.body.fat
+    });
 
-  user.save(function(err) {
-    if (err)
-      res.send(err);
+    user.save(function(err) {
+        if (err)
+        res.send(err);
 
-    res.json({ message: 'New User in this project!',
-                "id": user.id,
-                "username": user.username,
-                "password": user.password,
-                "sex": user.sex,
-                "weight": user.weight,
-                "height": user.height,
-                "birthdate": user.birthdate,
-                "createAt": user.birthdate,
-                "type": user.type,
-                "bmr": user.bmr,
-                "bmi": user.bmi,
-                "carbo": user.carbo,
-                "protein": user.protein,
-                "fat": user.fat
+        res.json({ message: 'New User in this project!',
+        "id": user.id,
+        "username": user.username,
+        "password": user.password,
+        "sex": user.sex,
+        "weight": user.weight,
+        "height": user.height,
+        "birthdate": user.birthdate,
+        "createAt": user.birthdate,
+        "type": user.type,
+        "bmr": user.bmr,
+        "bmi": user.bmi,
+        "carbo": user.carbo,
+        "protein": user.protein,
+        "fat": user.fat
 
 
-            });
-  });
+    });
+});
 };
 
 // Create endpoint /api/users for GET
 exports.getUsers = function(req, res) {
-  User.find(function(err, users) {
-    if (err)
-      res.send(err);
+    User.find(function(err, users) {
+        if (err)
+        res.send(err);
 
-    res.json({ result: users });
-  });
+        res.json({ result: users });
+    });
 };
 
 exports.getUser = (function(req, res){
--- models/user.js
+++ models/user.js
@@ -4,74 +4,74 @@ var bcrypt = require('bcrypt-nodejs');
 
 // Define our user schema
 var UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  sex: {
-    type: String
-  },
-  weight: {
-    type: Number
-  },
-  height: {
-    type: Number
-  },
-  bmr: {
-    type: Number
-  },
-  bmi: {
-    type: Number
-  },
-  birthdate: {
-    type: Date
-  },
-  carbo: {
-    type: Number
-  },
-  protein: {
-    type: Number
-  },
-  fat: {
-    type: Number
-  },
-  createAt: {
-    type: Date, default: Date.now
-  },
-  type: {
-    type: String, default: "Android"
-  }
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    sex: {
+        type: String
+    },
+    weight: {
+        type: Number
+    },
+    height: {
+        type: Number
+    },
+    bmr: {
+        type: Number
+    },
+    bmi: {
+        type: Number
+    },
+    birthdate: {
+        type: Date
+    },
+    carbo: {
+        type: Number
+    },
+    protein: {
+        type: Number
+    },
+    fat: {
+        type: Number
+    },
+    createAt: {
+        type: Date, default: Date.now
+    },
+    type: {
+        type: String, default: "Android"
+    }
 });
 
 // Execute before each user.save() call
 UserSchema.pre('save', function(callback) {
-  var user = this;
+    var user = this;
 
-  // Break out if the password hasn't changed
-  if (!user.isModified('password')) return callback();
+    // Break out if the password hasn't changed
+    if (!user.isModified('password')) return callback();
 
-  // Password changed so we need to hash it
-  bcrypt.genSalt(5, function(err, salt) {
-    if (err) return callback(err);
+    // Password changed so we need to hash it
+    bcrypt.genSalt(5, function(err, salt) {
+        if (err) return callback(err);
 
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return callback(err);
-      user.password = hash;
-      callback();
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) return callback(err);
+            user.password = hash;
+            callback();
+        });
     });
-  });
 });
 
 UserSchema.methods.verifyPassword = function(password, cb) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
 };
 
 // Export the Mongoose model
